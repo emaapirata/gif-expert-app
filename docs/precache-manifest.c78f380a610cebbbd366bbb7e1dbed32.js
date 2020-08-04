9738e8e0c41f65e4cc356c99bc37b653ff3d74c4
emaapirata <emaapirata@gmail.com>
2020-08-04 10:53:42 -0300

    docs agregado

---
+++ docs/precache-manifest.c78f380a610cebbbd366bbb7e1dbed32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f36cc715220b0d34544be6ca92a8eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b0338a328b57243c92",
+    "url": "/static/css/main.f521362e.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b0338a328b57243c92",
+    "url": "/static/js/main.fa2fde4c.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
